21276e170c73fe4d47c3e49f8ada8c7bc0ab511e
JCY-3958 <58291274+JCY-3958@users.noreply.github.com>
2024-03-04 17:15:26 +0900

    Add files via upload

--- JSPBook/src/main/java/ch12/com/filter/AuthenFilter.java
+++ JSPBook/src/main/java/ch12/com/filter/AuthenFilter.java
@@ -11,27 +11,29 @@ import javax.servlet.ServletResponse;
 
 public class AuthenFilter implements Filter{
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException{
-		System.out.println("Filter01 초기화...");
+	public void init(FilterConfig filterConfig) throws ServletException {
+			System.out.println("Filter01 초기화...");
 	}
+	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain filterChain) throws IOException, ServletException{
+	public void doFilter(javax.servlet.ServletRequest request, ServletResponse response,
+			javax.servlet.FilterChain filterChain) throws IOException, ServletException {
 		System.out.println("Filter01.jsp 수행...");
-		String name  =request.getParameter("name");
+		String name = request.getParameter("name");
 		
 		if(name == null || name.equals("")) {
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("text/html; charset=utf-8");
 			PrintWriter writer = response.getWriter();
-			String message = "입력된 name 값은 null입니다.";
+			String message = "입력된 name 값은 null 입니다.";
 			writer.println(message);
 			return;
 		}
 		filterChain.doFilter(request, response);
 	}
+	
 	@Override
 	public void destroy() {
-		System.out.println("Filter01 해제...");
+		System.out.println("Filter01 해체...");
 	}
 }
